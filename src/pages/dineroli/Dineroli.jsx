4e35598a6fc4cf8b70c7d03c3354bd93f13508a7
Emmanuel uma <umaemmanuel62@gmail.com>
2023-09-22 18:30:12 +0100

    libery in progress

--- src/pages/dineroli/Dineroli.jsx
+++ src/pages/dineroli/Dineroli.jsx
@@ -13,15 +13,15 @@ const Dineroli = () => {
         <a href="https://en.wikipedia.org/wiki/Blockchain" target="_blank" rel="noopener noreferrer">1.3 What is blockchain?</a>
         <a href="https://www.forbes.com/advisor/investing/cryptocurrency/crypto-glossary/" target="_blank" rel="noopener noreferrer">1.4 Crypto terms and meanings</a>
         <a href="https://www.blockchain-council.org/blockchain/types-of-crypto-wallets-explained/" target="_blank" rel="noopener noreferrer">1.5 Crypto wallets and types</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.6 Crypto exchanges and types</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.7 Ways to make money from cryptocurrency</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.8 Difference between coins and Tokens</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.9 Altcoins</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.10 Cryptocurrency Exchanges</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.11 CEX vs DEX</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.12 Security Best Practices</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.13 Public and Private Keys</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">1.14 Transactions and Addresses</a>
+        <a href="https://wundertrading.com/journal/en/learn/article/types-of-cryptocurrency-exchanges" target="_blank" rel="noopener noreferrer">1.6 Crypto exchanges and types</a>
+        <a href="https://www.stilt.com/blog/2021/06/how-to-make-money-with-cryptocurrency/" target="_blank" rel="noopener noreferrer">1.7 Ways to make money from cryptocurrency</a>
+        <a href="https://www.ledger.com/academy/crypto/what-is-the-difference-between-coins-and-tokens#:~:text=KEY%20TAKEAWAYS%3A,interesting%20properties%20relating%20to%20interoperability." target="_blank" rel="noopener noreferrer">1.8 Difference between coins and Tokens</a>
+        <a href="https://www.fool.com/terms/a/altcoin/#:~:text=Altcoins%20(alternative%20coins)%20is%20a,there%20are%20thousands%20of%20them." target="_blank" rel="noopener noreferrer">1.9 Altcoins</a>
+        <a href="https://www.forbes.com/advisor/in/investing/cryptocurrency/what-is-a-crypto-exchange/#:~:text=Show%20more-,Meaning%20of%20A%20Cryptocurrency%20Exchange,functions%20similarly%20like%20e%2Dbrokerages." target="_blank" rel="noopener noreferrer">1.10 Cryptocurrency Exchanges</a>
+        <a href="https://zebpay.com/blog/centralised-exchanges-vs-decentralised-exchanges#:~:text=CEX%20vs%20DEX-,What%20Is%20The%20Difference%20Between%20A%20Cex%20And%20A%20Dex,rely%20on%20a%20central%20authority." target="_blank" rel="noopener noreferrer">1.11 CEX vs DEX</a>
+        <a href="https://www.financemagnates.com/cryptocurrency/education-centre/crypto-wallet-security-and-best-practices/" target="_blank" rel="noopener noreferrer">1.12 Security Best Practices</a>
+        <a href="https://www.gemini.com/cryptopedia/public-private-keys-cryptography#:~:text=A%20public%20key%20allows%20you,cryptocurrency%20received%20in%20the%20transaction." target="_blank" rel="noopener noreferrer">1.13 Public and Private Keys</a>
+        <a href="https://www.bitcoin.com/get-started/how-bitcoin-transactions-work/" target="_blank" rel="noopener noreferrer">1.14 Transactions and Addresses</a>
         </article>
 
         <h1>2.0 DEFI</h1>
