ca8271c274cd0fa657e4b68291e13a3b31dca45b
Emmanuel uma <umaemmanuel62@gmail.com>
2023-10-21 07:47:06 +0100

    fixed the footer icon placement

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -5,7 +5,7 @@ import {AiOutlineInstagram} from 'react-icons/ai'
 import {FaTwitter} from 'react-icons/fa'
 import {TbNeedleThread} from 'react-icons/tb'
 import {FaYoutube} from 'react-icons/fa'
-import img from '../../asset/Dinero black.jpg'
+import img from '../../asset/Dinero_black-removebg-preview.png'
  
 
 const Footer = () => {
@@ -23,12 +23,12 @@ const Footer = () => {
             <div className="box img">
                 <img src={img} alt="" />
             </div>
-            <div className="box box-border">
-            <a href="mailto:dinerocommunity@gmail.com" target="_blank" rel="noopener noreferrer"><MdOutlineEmail className='icons' />mail</a>
-            <a href="https://www.instagram.com/direct/t/17848597500009533" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram className='icons' />instagram</a>
-            <a href="https://twitter.com/Dineroxyz" target="_blank" rel="noopener noreferrer"><FaTwitter className='icons' />X</a>
-            <a href="https://www.threads.net/@dineroxyz" target="_blank" rel="noopener noreferrer"><TbNeedleThread className='icons' />threads</a>
-            <a href="https://www.youtube.com/@Dineroxyz" target="_blank" rel="noopener noreferrer"><FaYoutube className='icons' />youtube</a>
+            <div className="box button">
+            <a href="mailto:dinerocommunity@gmail.com" target="_blank" rel="noopener noreferrer"><MdOutlineEmail className='icons' /></a>
+            <a href="https://www.instagram.com/direct/t/17848597500009533" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram className='icons' /></a>
+            <a href="https://twitter.com/Dineroxyz" target="_blank" rel="noopener noreferrer"><FaTwitter className='icons' /></a>
+            <a href="https://www.threads.net/@dineroxyz" target="_blank" rel="noopener noreferrer"><TbNeedleThread className='icons' /></a>
+            <a href="https://www.youtube.com/@Dineroxyz" target="_blank" rel="noopener noreferrer"><FaYoutube className='icons' /></a>
             </div>
             <div className="box box-border">
                 <p>© 2023 Dinero. All rights reserved.</p>
